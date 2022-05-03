f513c96ef59c1c34108872e2b3d7685418247786
teghfo <teghfo@gmail.com>
2022-05-03 17:52:38 +0430

    some instruction for django models

--- model_train/app/admin.py
+++ model_train/app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import Product, Cart, Person
+
+admin.site.register(Product)
+admin.site.register(Cart)
+admin.site.register(Person)
--- model_train/app/models.py
+++ model_train/app/models.py
@@ -9,4 +9,39 @@ class Person(models.Model):
         ordering = ['-first_name']
     
     def __str__(self) -> str:
-        return self.first_name + " " + self.last_name 
+        return self.first_name + " " + self.last_name
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    description = models.TextField(null=True, blank=True)
+    exist_qty = models.IntegerField()
+    moshakhast = models.JSONField(default=dict)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(Person, on_delete=models.CASCADE)
+    product = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return self.user.first_name
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.RESTRICT)
+    user = models.ForeignKey(Person, on_delete=models.CASCADE)
+    qty = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"kharid {self.user.first_name}"
