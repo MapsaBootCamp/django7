1ea790972df8c4819301790087d2e2525e6a5c92
teghfo <teghfo@gmail.com>
2022-05-19 15:28:09 +0430

    generic views

--- library/book/urls.py
+++ library/book/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from .views import BookList, BookDetail, book_list, book_detail, add_comment
+from .views import BookList, BookDetail, book_list, book_detail, add_comment, BookListV1, BookDetailV2
 
 
 urlpatterns = [
     path("add-comment/<int:book_id>", add_comment, name="book_add_comment"),
+    # path('book-list/', BookListV1.as_view(), name="book_list_template"),
     path('book-list/', book_list, name="book_list_template"),
-    path('book-detail/<int:id>', book_detail, name="book_detail_template"),
+    path('book-detail/<int:id>', BookDetailV2.as_view(), name="book_detail_template"),
     path('api/book-list/', BookList.as_view(), name="book_list"),
     path('api/book-detail/<int:id>', BookDetail.as_view(), name="book_detail"),
 ]
--- library/book/views.py
+++ library/book/views.py
@@ -7,6 +7,8 @@ from django.views import View
 from django.forms.models import model_to_dict
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import ListView, FormView, DeleteView, DetailView
+from django.contrib.auth.decorators import login_required
 
 from .models import Book, Category, Comment
 from .forms import CommentForm
@@ -36,22 +38,31 @@ def index(request):
     cat_qs = Category.objects.all()
     return render(request, "book/index.html", {"categories": cat_qs})
 
+
+@login_required(login_url="/user/login/")
 def book_list(request: HttpRequest):
+    ## TODO: handle SQL ingection
     request.session["name"] = "Gholam"
+    q = request.GET.get("search")
     book_qs = Book.objects.all()
+    if q:
+        book_qs = book_qs.filter(title__contains=q)
     return render(request, "book/book_list.html", {"books": book_qs})
 
 
-def book_detail(request, id):
-    book = get_object_or_404(Book, id=id)
-    comments = book.comments.all()
-    comment_form = CommentForm()
-    context = {
-        "book": book, 
-        "comments": comments,
-        "comment_form": comment_form
-        }
-    return render(request, "book/book_detail.html", context)
+class BookListV1(ListView):
+    model = Book
+    template_name: str =  "book/book_list.html"
+    context_object_name = "books"
+    paginate_by = 2
+
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        q = self.request.GET.get("search")
+        if q:
+            qs = qs.filter(title__contains=q)
+        return qs
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -86,3 +97,31 @@ class BookDetail(View):
     def get(self, request, id):
         book_obj: Book = get_object_or_404(Book, id=id)
         return JsonResponse(model_to_dict(book_obj))
+
+
+def book_detail(request, id):
+    book = get_object_or_404(Book, id=id)
+    comments = book.comments.all()
+    comment_form = CommentForm()
+    context = {
+        "book": book, 
+        "comments": comments,
+        "comment_form": comment_form
+        }
+    return render(request, "book/book_detail.html", context)
+
+
+class BookDetailV2(DetailView):
+    model = Book
+    template_name: str = "book/book_detail.html"
+    context_object_name = "book"
+    pk_url_kwarg = "id"
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        comments = self.get_object().comments.all()
+        comment_form = CommentForm()
+        context["comments"]  = comments
+        context["comment_form"] = comment_form
+        return context
