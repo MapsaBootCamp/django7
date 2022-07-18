68d5bfc872fb427c7484efcd8dc44b894262e822
teghfo <teghfo@gmail.com>
2022-07-18 11:41:29 +0430

    index user model on username column

--- messenger_fastapi/config/db.py
+++ messenger_fastapi/config/db.py
@@ -1,4 +1,5 @@
 import motor.motor_asyncio
+from pymongo import TEXT
 
 MONGO_DETAILS = "mongodb://localhost:27017"
 
@@ -7,3 +8,5 @@ client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 database = client.messenger
 
 users_collection = database.get_collection("users")
+
+users_collection.create_index([('username', TEXT)], default_language='english')
