05afd711656c37194adf9ba4464ea367c5543b3b
teghfo <teghfo@gmail.com>
2022-07-18 11:00:26 +0430

    add global dependency for ip validation

--- messenger_fastapi/app.py
+++ messenger_fastapi/app.py
@@ -1,9 +1,16 @@
 import time
  
-from fastapi import FastAPI, Request
+from fastapi import Depends, FastAPI, Form, Request, HTTPException
 from api.User import router as user_router
+from api.chat import router as chat_router
 
-app = FastAPI()
+async def verify_ip(request: Request):
+    if request.client.host == "iran_ip":
+        raise HTTPException(status_code=403, detail="Forbidon")
+    return request
+
+app = FastAPI(dependencies=[Depends(verify_ip)])
+
 
 @app.middleware("http")
 async def add_process_time_header(request: Request, call_next):
@@ -14,8 +21,13 @@ async def add_process_time_header(request: Request, call_next):
     response.headers["X-Process-Time"] = str(process_time)
     return response
 
+@app.post("/login/")
+async def login(username: str = Form(), password: str = Form()):
+    return {"username": username}
 
+    
 app.include_router(user_router, prefix="/api")
+app.include_router(chat_router)
 
 
 # TODO: model? baraye messenger
