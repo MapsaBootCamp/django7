09c1ad2cf6dc8514af6a7ca3fe84e9b185d75d23
teghfo <teghfo@gmail.com>
2022-07-18 11:40:08 +0430

    update User models

--- messenger_fastapi/models/User.py
+++ messenger_fastapi/models/User.py
@@ -1,5 +1,5 @@
 from typing import Union
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 
@@ -7,7 +7,24 @@ class User(BaseModel):
     username: str
     firstname: Union[str, None] = None
     lastname: Union[str, None] = None
+    age: Union[int, None] = Field(default=None, gt=0)
+
+
+class UserUpdate(BaseModel):
+    firstname: Union[str, None] = None
+    lastname: Union[str, None] = None
+    age: Union[int, None] = Field(default=None, gt=0)
 
 
 class UserInDB(User):
     password: str
+
+
+class UserOut(BaseModel):
+    username: str 
+    firstname: Union[str, None] = None
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
