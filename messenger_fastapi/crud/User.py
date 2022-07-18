5992833ac71b6e5b85ea38073e698973560aa5a8
teghfo <teghfo@gmail.com>
2022-07-18 11:40:32 +0430

    save hashed password in db

--- messenger_fastapi/crud/User.py
+++ messenger_fastapi/crud/User.py
@@ -1,6 +1,9 @@
 from config.db import users_collection
+from models.User import User
 from schema.User import user_helper
 from bson.objectid import ObjectId
+
+from utils import get_password_hash, verify_password
 
 # Retrieve all students present in the database
 async def retrieve_users():
@@ -9,9 +12,9 @@ async def retrieve_users():
         users.append(user_helper(user))
     return users
 
-
 # Add a new student into to the database
 async def add_user(user_data: dict) -> dict:
+    user_data["password"] = get_password_hash(user_data["password"])
     user = await users_collection.insert_one(user_data)
     new_user = await users_collection.find_one({"_id": user.inserted_id})
     return user_helper(new_user)
@@ -25,11 +28,11 @@ async def retrieve_user(id: str) -> dict:
         raise Exception("user not found")
 
 # Update a student with a matching ID
-async def update_user(id: str, data: dict):
-    user = await users_collection.find_one({"_id": ObjectId(id)})
+async def update_user(user: User, data: dict):
+    user = await users_collection.find_one({"username": user["username"]})
     if user:
         updated_user = await users_collection.update_one(
-            {"_id": ObjectId(id)}, {"$set": data}
+            {"username": user["username"]}, {"$set": data}
         )
         return updated_user
     else:
@@ -42,8 +45,14 @@ async def delete_user(id: str):
         await users_collection.delete_one({"_id": ObjectId(id)})
         return True
 
-
-
+async def authenticate_user(username: str, password: str):
+    user = await users_collection.find_one({"username": username})
+    print(user)
+    if not user:
+        return False
+    if not verify_password(password, user["password"]):
+        return False
+    return user
 
 
 
